dbac40c86fb23322b08e87180b16999fd11e8a88
Jo Yie <joyieleong@gmail.com>
2024-10-06 23:42:02 +0800

    implemented day 3 workshop

--- src/App.java
+++ src/App.java
@@ -1,10 +1,29 @@
 import java.io.Console;
 import java.util.Scanner;
 
+import java.io.File;
+
 public class App {
     
     public static void main(String[] args) {
-        ShoppingCart userCart = new ShoppingCart();
+        String directoryName = "db"; // default directory 
+
+        // check if a directory was provided 
+        if (args.length > 0) {
+            directoryName = args[0];
+        }
+
+        // create the directory if it does not exist 
+        File directory = new File(directoryName);
+        if (!directory.exists()) {
+            directory.mkdirs(); 
+            System.out.println("Directory " + directoryName + " created");
+        } else {
+            System.out.println("Using directory " + directoryName);
+        }
+
+
+        ShoppingCart userCart = new ShoppingCart(directoryName);
         System.out.println("Welcome to your shopping cart");
 
         Console console = System.console(); 
@@ -40,6 +59,28 @@ public class App {
 
                 userCart.delete(Integer.parseInt(deleteIndex));
                 
+            }
+
+            // input starts with "login"
+
+            else if (keyboardInput.startsWith("login")) {
+                // ShoppingCart userCart = new ShoppingCart(directoryName);
+
+                Scanner scan = new Scanner(keyboardInput.substring(6));
+                String inputName = scan.next();
+                scan.close();
+
+                userCart.login(inputName);
+            }
+
+            // input: "save"
+            else if (keyboardInput.equals("save")) {
+                userCart.save();
+            }
+
+            // input: "users"
+            else if (keyboardInput.equals("users")) {
+                userCart.userList();
             }
 
         }
--- src/shoppingCart.java
+++ src/shoppingCart.java
@@ -1,12 +1,20 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class ShoppingCart {
 
     private List<String> cart = new ArrayList<>();
+    private String directoryName;
+    private String user;
     
-    public ShoppingCart() {
+    public ShoppingCart(String directoryName) {
+        this.directoryName = directoryName;
     }
 
+    // returns formatted list of items in cart
     public void list() {
         if (this.cart.size() > 0) {
             int i = 1;
@@ -16,10 +24,10 @@ public class ShoppingCart {
             }
         } else {
             System.out.println("Your cart is empty");
-        }
-        
+        } 
     }
 
+    // adds items to cart
     public void add(String item) {
         if (this.cart.contains(item)) {
             System.out.println("You have " + item + " in your cart");
@@ -30,6 +38,7 @@ public class ShoppingCart {
         
     }
 
+    // deletes items from cart 
     public void delete(int num) {
         if (num <= getCart().size() && num > 0) {
             int indexToDelete = num - 1; 
@@ -46,6 +55,69 @@ public class ShoppingCart {
 
     public void setCart(List<String> cart) {
         this.cart = cart;
+    }
+
+    
+    // loads user database file from shopping cart directory
+    // if database file does not exist, file is created
+    public void login(String userName) {
+        File userFile = new File(this.directoryName + "/" + userName + ".db");
+
+        if (userFile.exists()) {
+            if (this.cart.size() > 0) {
+                System.out.println(userName + ", your cart contains the following items");
+                ((ShoppingCart) cart).list();
+            } else {
+                System.out.println(userName + ", your cart is empty");
+            }
+        } else {
+            try {
+                userFile.createNewFile(); 
+                System.out.println(userName + ", your cart is empty");
+            } catch (IOException e) {
+                System.out.println("Error creating user file");
+            }
+        }
+        this.user = userName;
+
+    }
+
+    // saves users cart to file 
+    // prompts login if not logged in
+    public void save() {
+        if (user.equals("")) {
+            System.out.println("Please login to save");
+        } else {
+            File userFile = new File(directoryName + "/" + user + ".db");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(userFile))) {
+            for (String item : cart) {
+                writer.write(item);
+                writer.newLine();
+            }
+            System.out.println("Your cart has been saved");
+        } catch (IOException e) {
+            System.out.println("Error saving cart");
+            }
+        }
+
+    }
+
+    // lists all filenames under shopping cart directory
+    public void userList() {
+        File directory = new File(directoryName);
+        File[] files = directory.listFiles((dir, name) -> name.endsWith(".db"));
+        
+        if (files != null && files.length > 0) {
+            System.out.println("The following users are registered");
+            int num = 1;
+            for (File f : files) {
+                String user = f.getName().replace(".db", "");
+                System.out.printf("%d. %s \r\n", num, user);
+                num ++;
+            } 
+        } else {
+            System.out.println("No users found");
+        }
     }
 
     
